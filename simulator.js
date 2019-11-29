3eb090e1c71d1899e9c057ef67f5fca85394969e
Mike Ly <mike.ly@deliveroo.co.uk>
2019-11-29 16:07:53 +0000

    Listen to certain commands from the simulator

--- simulator.js
+++ simulator.js
@@ -8,9 +8,63 @@ class Simulator {
   init() {
     this.canvasContext.fillRect(50, 25, 150, 100);
 
-    eventManager.subscribe("sendCommand", (command) => {
-      console.warn('########### sendCommand', command);
+    eventManager.subscribe("sendCommand", (commandString) => {
+      const [command, args] = commandString.split(" ");
+      this.simulateCommand(command, args);
     });
+  }
+
+  simulateCommand(command, args) {
+    console.warn('### simulateCommand', command, args);
+    switch (command) {
+      case 'start': {
+        break;
+      }
+      case 'takeoff': {
+        break;
+      }
+      case 'land': {
+        break;
+      }
+      case 'emergency': {
+        break;
+      }
+      case 'streamon': {
+        break;
+      }
+      case 'streamoff': {
+        break;
+      }
+      case 'forward': {
+        break;
+      }
+      case 'back': {
+        break;
+      }
+      case 'left': {
+        break;
+      }
+      case 'right': {
+        break;
+      }
+      case 'up': {
+        break;
+      }
+      case 'down': {
+        break;
+      }
+      case 'CCW': {
+        break;
+      }
+      case 'CW': {
+        break;
+      }
+      case 'flip': {
+        break;
+      }
+      default:
+        console.warn(`Command can't be simulated`, command);
+    }
   }
 }
 
