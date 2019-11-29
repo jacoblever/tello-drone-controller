0ba43fa7999a5431c60fd56d11c0f5adbeae94cf
Mike Ly <mike.ly@deliveroo.co.uk>
2019-11-29 16:39:27 +0000

    Render a background

--- simulator.js
+++ simulator.js
@@ -1,17 +1,42 @@
+const gridSize = 20;
+
 class Simulator {
   constructor(cavasId) {
-    this.canvasContext = document.getElementById(cavasId).getContext('2d');
+    this.canvas = document.getElementById(cavasId);
+    this.canvasContext = this.canvas.getContext('2d');
 
     this.init();
   }
 
   init() {
-    this.canvasContext.fillRect(50, 25, 150, 100);
+    this.renderBackground();
 
     eventManager.subscribe("sendCommand", (commandString) => {
       const [command, args] = commandString.split(" ");
       this.simulateCommand(command, args);
     });
+  }
+
+  renderBackground() {
+    this.canvasContext.fillStyle = this.canvas.getAttribute('backgroundColor') || "#f0f0f0";
+    this.canvasContext.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.canvasContext.fillStyle = this.canvas.getAttribute('gridColor') || "#ccc";
+    this.renderGrid();
+  }
+
+  renderGrid() {
+    for (var x = 0; x < this.canvas.width; x += gridSize) {
+      this.canvasContext.moveTo(x, 0);
+      this.canvasContext.lineTo(x, this.canvas.height);
+    }
+    
+    for (var y = 0; y < this.canvas.height; y += gridSize) {
+      this.canvasContext.moveTo(0, y);
+      this.canvasContext.lineTo(this.canvas.width, y);
+    }
+
+    this.canvasContext.strokeStyle = "#ddd";
+    this.canvasContext.stroke();
   }
 
   simulateCommand(command, args) {
